c1c74755bfce08a70d2e938568be5436c4356571
Abubakar <abubakarrmalik383@gmail.com>
2025-12-22 00:50:50 +0500

    Add contact API

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,24 +3,10 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
-  throw new Error("❌ MONGODB_URI is not defined");
+  throw new Error("❌ MONGODB_URI is not defined in environment variables");
 }
 
-const options = {};
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
-}
-
-clientPromise = global._mongoClientPromise;
+const client = new MongoClient(uri);
+const clientPromise = client.connect();
 
 export default clientPromise;
